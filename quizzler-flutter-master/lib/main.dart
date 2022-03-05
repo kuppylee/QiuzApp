2a4b803584150101b1df7610c3d54d74294b6f1c
Mobolaji <bjkuppy@gmail.com>
2022-03-05 22:05:54 +0100

    Made some abstraction with the quiz question and encapsulated it

--- quizzler-flutter-master/lib/main.dart
+++ quizzler-flutter-master/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
 
 void main() => runApp(Quizzler());
 
@@ -27,13 +28,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizBrain questionBrain = QuizBrain();
 
   List <Icon> scoreKeeper = [];
-  List <Question> questionBank = [
-    Question(q:'You can lead a cow down stairs but not up stairs...', a: false ),
-    Question(q:'Approximately one quarter of human bones are in the feet.', a: true ),
-    Question(q:'A slug\'s blood is green.', a: true ),
-  ];
+
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -47,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                questionBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -70,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswers = questionBank[questionNumber].questionAnswer;
+                bool correctAnswers = questionBrain.getCorrectAnswer(questionNumber);
                 if(correctAnswers == true){
                   scoreKeeper.add(Icon (Icons.check, color: Colors.green,));
                 }else{
@@ -79,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
                 setState(() {
                   questionNumber++;
-                  if(questionNumber == questionBank.length){
+                  if(questionNumber == questionBrain.getQuestionCount()){
                     questionNumber = 0;
                   }
                 });
@@ -100,7 +98,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswers = questionBank[questionNumber].questionAnswer;
+                bool correctAnswers = questionBrain.getCorrectAnswer(questionNumber);
                 if(correctAnswers == false){
                   scoreKeeper.add(Icon (Icons.check, color: Colors.green,));
                 }else{
@@ -109,7 +107,7 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 setState(() {
                   questionNumber++;
-                  if(questionNumber == questionBank.length){
+                  if(questionNumber == questionBrain.getQuestionCount()){
                     questionNumber = 0;
                   }
                 });
